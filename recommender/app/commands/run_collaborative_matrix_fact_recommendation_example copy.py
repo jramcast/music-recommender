0b99b25cc7a5e57a43b5181cec7c04e5efbc4f21
Jaime Ramírez <jaime.ram@gmail.com>
2020-04-07 09:09:29 +0200

    Started matrix factorization recommender example

---
+++ recommender/app/commands/run_collaborative_matrix_fact_recommendation_example copy.py
@@ -0,0 +1,6 @@
+"""
+Basic Collaborative Filtering Tutorial (Matrix factorization)
+
+This tutorial is based on
+https://heartbeat.fritz.ai/recommender-systems-with-python-part-iii-collaborative-filtering-singular-value-decomposition-5b5dcb3f242b
+"""
