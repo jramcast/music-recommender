bbfd2743edd3261faf4d5d485bbc233a9904e779
Jaime Ramírez <jaime.ram@gmail.com>
2020-04-14 09:44:15 +0200

    Created empty file to experiment with MSD

---
+++ recommender/app/commands/run_msd_recommendation.py
@@ -0,0 +1,6 @@
+"""
+TODO: Create a recommender system for Million Song dataset (MSD) inspired in this 2 sources:
+
+- Efficient top-n recommendation for very large scale binary rated datasets: https://dl.acm.org/doi/pdf/10.1145/2507157.2507189
+- Million Song Dataset Challenge: https://www.kaggle.com/c/msdchallenge
+"""
