e0fe0003c6e5e42932530c7e3dc8c359dcddb5c5
Jaime Ramírez <jaime.ram@gmail.com>
2020-04-22 09:20:54 +0200

    Getting msd challenge data

--- recommender/app/commands/msd/run_todo.py
+++ recommender/app/commands/msd/run_todo.py
@@ -7,6 +7,8 @@ Recommender system for Million Song dataset (MSD),
 - Million Song Dataset Challenge:
     https://www.kaggle.com/c/msdchallenge
 
+- Get the dataset from: http://millionsongdataset.com/challenge/#data1
+
 In particular, this experiment ....
 """
 
@@ -16,3 +18,22 @@ In particular, this experiment ....
 # TODO: check song to track mapping with "taste_profile_song_to_tracks.txt"
 # It seems songs are not the same as tracks
 # More info here: https://www.kaggle.com/c/msdchallenge/data
+
+"""
+DATA Review:
+
+The concatenation of EvalDataYear1MSDWebsite/*_visible seems to be 
+the same as kaggle_challenge_files/kaggle_visible_evaluation_triplets.txt (check this)
+Could this be our validation set?
+
+My guess:
+    - Public leaderboard uses: year1_valid_triplets_hidden.txt
+    - Private leaderboard uses: year1_test_triplets_hidden.txt
+    - EvalDataYear1MSDWebsite/*_visible seems to be 
+the same as kaggle_challenge_files/kaggle_visible_evaluation_triplets.txt (for local testing)
+
+
+EVALUATIN Metric (mAP):
+
+https://www.kaggle.com/c/msdchallenge/discussion/1860
+"""
